5ee758c7640c8b2b2f2adf2c80e3caca44824353
Starck43 <saloon.as@gmail.com>
2023-01-06 17:19:59 +0200

    updated photo

---
+++ src/img/starck-theme/front-page.php
@@ -0,0 +1,155 @@
+<?php
+
+	get_header();
+	$url = get_stylesheet_directory_uri();
+?>
+
+	<section id="content">
+		<article id="members">
+			<header id="members-header"><h1 class="title">Члены гильдии</h1></header>
+			<div id="members-content" class="row">
+				<div id="member-1" class="member-container col-sm-6 col-md-3 "><a href="andreevsky">
+					<img src="<?php echo $url; ?>/img/placeholder-300.png" data-src="<?php echo $url; ?>/img/@2x/andreevsky.jpg" data-srcset="<?php echo $url; ?>/img/@1x/andreevsky.jpg 50px, <?php echo $url; ?>/img/@2x/andreevsky.jpg 300px" data-sizes="auto" alt="Андрей Андреевский" class="member-image lazy"/>
+					<h2 class="member-title">Андрей
+					Андреевский</h2>
+				</a></div>
+				<div id="member-2" class="member-container col-sm-6 col-md-3 "><a href="isupova">
+					<img src="<?php echo $url; ?>/img/placeholder-300.png" data-src="<?php echo $url; ?>/img/@2x/isupova.jpg" data-srcset="<?php echo $url; ?>/img/@1x/isupova.jpg 150px, <?php echo $url; ?>/img/@2x/isupova.jpg 300px" data-sizes="auto" alt="Анна
+					Исупова" class="member-image lazy">
+					<h2 class="member-title">Анна
+					Исупова</h2>
+				</a></div>
+				<div id="member-3" class="member-container col-sm-6 col-md-3 "><a href="kigel">
+					<img src="<?php echo $url; ?>/img/placeholder-300.png" data-src="<?php echo $url; ?>/img/@2x/kigel.jpg" data-srcset="<?php echo $url; ?>/img/@1x/kigel.jpg 150px, <?php echo $url; ?>/img/@2x/kigel.jpg 300px" data-sizes="auto" alt="Ирина
+					Кигель" class="member-image lazy">
+					<h2 class="member-title">Ирина
+					Кигель</h2>
+				</a></div>
+				<div id="member-4" class="member-container col-sm-6 col-md-3 "><a href="strizhova">
+					<img src="<?php echo $url; ?>/img/placeholder-300.png" data-src="<?php echo $url; ?>/img/@2x/strizhova.jpg" data-srcset="<?php echo $url; ?>/img/@1x/strizhova.jpg 150px, <?php echo $url; ?>/img/@2x/strizhova.jpg 300px" data-sizes="auto" alt="Анна
+					Стрижова" class="member-image lazy">
+					<h2 class="member-title">Анна
+					Стрижова</h2>
+				</a></div>
+				<div id="member-5" class="member-container col-sm-6 col-md-3 "><a href="rozhnev">
+					<img src="<?php echo $url; ?>/img/placeholder-300.png" data-src="<?php echo $url; ?>/img/@2x/rozhnev.jpg" data-srcset="<?php echo $url; ?>/img/@1x/rozhnev.jpg 150px, <?php echo $url; ?>/img/@2x/rozhnev.jpg 300px" data-sizes="auto" alt="Олег
+					Рожнев" class="member-image lazy">
+					<h2 class="member-title">Олег
+					Рожнев</h2>
+				</a></div>
+				<div id="member-6" class="member-container col-sm-6 col-md-3 "><a href="kotenko">
+					<img src="<?php echo $url; ?>/img/placeholder-300.png" data-src="<?php echo $url; ?>/img/@2x/kotenko.jpg" data-srcset="<?php echo $url; ?>/img/@1x/kotenko.jpg 150px, <?php echo $url; ?>/img/@2x/kotenko.jpg 300px" data-sizes="auto" alt="Елена
+					Котенко" class="member-image lazy">
+					<h2 class="member-title">Елена
+					Котенко</h2>
+				</a></div>
+				<div id="member-14" class="member-container col-sm-6 col-md-3 "><a href="kotomchina">
+					<img src="<?php echo $url; ?>/img/placeholder-300.png" data-src="<?php echo $url; ?>/img/@2x/kotomchina.jpg" data-srcset="<?php echo $url; ?>/img/@1x/kotomchina.jpg 150px, <?php echo $url; ?>/img/@2x/kotomchina.jpg 300px" data-sizes="auto" alt="Елена
+					Котомчина" class="member-image lazy">
+					<h2 class="member-title">Елена
+					Котомчина</h2>
+				</a></div>
+				<div id="member-7" class="member-container col-sm-6 col-md-3 "><a href="denisenko">
+					<img src="<?php echo $url; ?>/img/placeholder-300.png" data-src="<?php echo $url; ?>/img/@2x/denisenko.jpg" data-srcset="<?php echo $url; ?>/img/@1x/denisenko.jpg 150px, <?php echo $url; ?>/img/@2x/denisenko.jpg 300px" data-sizes="auto" alt="Юлия
+					Денисенко" class="member-image lazy">
+					<h2 class="member-title">Юлия
+					Денисенко</h2>
+				</a></div>
+				<div id="member-8" class="member-container col-sm-6 col-md-3 "><a href="atnagulova">
+					<img src="<?php echo $url; ?>/img/placeholder-300.png" data-src="<?php echo $url; ?>/img/@2x/atnagulova.jpg" data-srcset="<?php echo $url; ?>/img/@1x/atnagulova.jpg 150px, <?php echo $url; ?>/img/@2x/atnagulova.jpg 300px" data-sizes="auto" alt="Ольга
+					Атнагулова" class="member-image lazy">
+					<h2 class="member-title">Ольга
+					Атнагулова</h2>
+				</a></div>
+				<div id="member-9" class="member-container col-sm-6 col-md-3 "><a href="elsukova">
+					<img src="<?php echo $url; ?>/img/placeholder-300.png" data-src="<?php echo $url; ?>/img/@2x/elsukova.jpg" data-srcset="<?php echo $url; ?>/img/@1x/elsukova.jpg 150px, <?php echo $url; ?>/img/@2x/elsukova.jpg 300px" data-sizes="auto" alt="Екатерина
+					Елсукова" class="member-image lazy">
+					<h2 class="member-title">Екатерина
+					Елсукова</h2>
+				</a></div>
+				<div id="member-10" class="member-container col-sm-6 col-md-3 "><a href="orlov">
+					<img src="<?php echo $url; ?>/img/placeholder-300.png" data-src="<?php echo $url; ?>/img/@2x/orlov.jpg" data-srcset="<?php echo $url; ?>/img/@1x/orlov.jpg 150px, <?php echo $url; ?>/img/@2x/orlov.jpg 300px" data-sizes="auto" alt="Сергей
+					Орлов" class="member-image lazy">
+					<h2 class="member-title">Сергей
+					Орлов</h2>
+				</a></div>
+				<div id="member-11" class="member-container col-sm-6 col-md-3 "><a href="laletin">
+					<img src="<?php echo $url; ?>/img/placeholder-300.png" data-src="<?php echo $url; ?>/img/@2x/laletin.jpg" data-srcset="<?php echo $url; ?>/img/@1x/laletin.jpg 150px, <?php echo $url; ?>/img/@2x/laletin.jpg 300px" data-sizes="auto" alt="Антон
+					Лалетин" class="member-image lazy">
+					<h2 class="member-title">Антон
+					Лалетин</h2>
+				</a></div>
+				<div id="member-12" class="member-container col-sm-6 col-md-3 "><a href="tregubova">
+					<img src="<?php echo $url; ?>/img/placeholder-300.png" data-src="<?php echo $url; ?>/img/@2x/tregubova.jpg" data-srcset="<?php echo $url; ?>/img/@1x/tregubova.jpg 150px, <?php echo $url; ?>/img/@2x/tregubova.jpg 300px" data-sizes="auto" alt="Ольга
+					Трегубова" class="member-image lazy">
+					<h2 class="member-title">Ольга
+					Трегубова</h2>
+				</a></div>
+				<div id="member-13" class="member-container col-sm-6 col-md-3 "><a href="chulkina">
+					<img src="<?php echo $url; ?>/img/placeholder-300.png" data-src="<?php echo $url; ?>/img/@2x/chulkina.jpg" data-srcset="<?php echo $url; ?>/img/@1x/chulkina.jpg 150px, <?php echo $url; ?>/img/@2x/chulkina.jpg 300px" data-sizes="auto" alt="Ирина
+					Чулкина" class="member-image lazy">
+					<h2 class="member-title">Ирина
+					Чулкина</h2>
+				</a></div>
+				<div id="member-14" class="member-container col-sm-6 col-md-3 "><a href="veinkrot">
+					<img src="<?php echo $url; ?>/img/placeholder-300.png" data-src="<?php echo $url; ?>/img/@2x/veinkrot.jpg" data-srcset="<?php echo $url; ?>/img/@1x/veinkrot.jpg 150px, <?php echo $url; ?>/img/@2x/veinkrot.jpg 300px" data-sizes="auto" alt="Алина
+					Вейнкрот" class="member-image lazy">
+					<h2 class="member-title">Алина
+					Вейнкрот</h2>
+				</a></div>
+			</div>
+		</article>
+		<article id="about">
+			<header id="about-header"><h1 class="title">О гильдии</h1></header>
+			<div id="about-content">
+				<?php
+				$query = new WP_Query( 'category_name=about-us' ); //выведем только посты с рубрикой О гильдии
+				if ( $query->have_posts() ) {
+					while ( $query->have_posts() ) {
+						$query->the_post();
+
+						edit_post_link();
+
+						the_content();
+					}
+					wp_reset_postdata();
+				}
+				?>
+			</div>
+		</article>
+		<article id="news">
+			<?php
+			$query = new WP_Query( 'category_name=news' ); //выведем только посты с рубрикой Новости
+			if ( $query->have_posts() ) {
+				?>
+				<header id="news-header"><h1 class="title">Новости</h1></header>
+				<div id="news-content">
+				<?php
+				while ( $query->have_posts() ) {
+					$query->the_post();
+					get_template_part( 'entry' );
+				}
+				?>
+				</div>
+				<?php
+				wp_reset_postdata();
+			}
+			?>
+		</article>
+		<article id="documents">
+			<header id="documents-header"><h1 class="title">Документы</h1></header>
+			<div id="documents-content">
+				<a href="admission" class="document-link"><i class="icon fa-stack icon-doc"></i><h2>Положение о приеме в гильдию дизайнеров кировской области</h2></a>
+				<a href="codex" class="document-link"><i class="icon fa-stack icon-doc"></i><h2>Кодекс профессиональной этики</h2></a>
+				<a href="price" class="document-link"><i class="icon fa-stack icon-doc"></i><h2>Рекомендуемый прейскурант</h2></a>
+			</div>
+		</article>
+		<article id="contacts">
+			<header id="contacts-header"><h1 class="title">Обратная связь</h1></header>
+			<div id="contacts-content"><!-- Связаться с нами можно по электронной почте: <a href="mailto:43design@mail.ru">43design@mail.ru</a> -->
+				<?php echo do_shortcode('[wpforms id="40" title="false" description="false"]'); ?>
+			</div>
+		</article>
+	</section>
+
+<?php get_footer(); ?>
